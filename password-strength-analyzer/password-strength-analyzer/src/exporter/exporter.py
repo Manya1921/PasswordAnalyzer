0df3235ff819cd79e2fb2d9b2d2dc1d4245b6004
Manya1921 <manyam08872@gmail.com>
2025-11-27 17:01:52 +0530

    Delete password-strength-analyzer directory

--- password-strength-analyzer/password-strength-analyzer/src/exporter/exporter.py
+++
@@ -1,5 +0,0 @@
-class WordlistExporter:
-    def export_to_txt(self, wordlist, filename):
-        with open(filename, 'w') as file:
-            for word in wordlist:
-                file.write(f"{word}\n")
